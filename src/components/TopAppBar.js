bd762cb2cb78189cdad67b550ef614e3bf4eeff8
inukun7 <40438541+inukun7@users.noreply.github.com>
2018-08-06 11:58:04 -0600

    almost done styling the top app bar

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import "../styles/TopAppBar.css";
 
 class TopAppBar extends Component {
   render() {
     return (
       <header className="App-header">
-        <h1 className="App-title">Tubular</h1>
+        <div className="App-title">Tubular</div>
+        <input type="text" className="App-searchbar" placeholder={"🔎 search"}  />
+        <button className="App-account">Sign in</button>
+        <button className="App-account">Log in</button>
       </header>
     );
   }
